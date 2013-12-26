a7d8a11b6bf89b10e9b108b1575e6a008aaf961c
Dave <gasolinewaltz@gmail.com>
2013-12-26 14:01:20 -0800

    new branch for async

--- app/async.js
+++ app/async.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+        //return new Promise
     },
 
     manipulateRemoteData : function(url) {
